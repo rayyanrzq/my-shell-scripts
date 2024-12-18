625a67d44da3ead578af95bc3db760ddf0ee2040
rayyanrzq <rayyanrazaq26@gmail.com>
2024-12-18 23:38:35 +0700

    give some context

--- film-copier.sh
+++ film-copier.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #
-#  film-copier - script to copy the latest movies on my local directory to
+#  film-copier - Copy the latest movies on my local directory to
 #  my external hdd
 #
 
+# Directory path
 origin="/home/rayyan/Videos/" 
 dest="/media/rayyan/78EC3A9BEC3A541A/Film"
 
-# Start copying commands with rsync
+# Copy with rsync
 sudo rsync --archive --verbose --progress --ignore-existing --exclude 'Camera/' $origin $dest

6d75c3f9a96ead190beeb911d440e01ad6811780
rayyanrzq <rayyanrazaq26@gmail.com>
2024-12-18 23:35:00 +0700

    clean up

--- vbox-guest-additions.sh
+++ vbox-guest-additions.sh
@@ -3,20 +3,15 @@
 # Script for Installing VBox Guest Additions
 #
 
-echo '***Install necessary package'
+echo
+echo '*** Install necessary package'
 echo
 sudo apt install gcc make perl bzip2 tar
 
-echo
-echo '***Mount guest additions image to /cdrom'
-echo
+# Mount guest additions image to /cdrom
 sudo mount /dev/sr0 /cdrom
 
 echo
 echo '***Start installing guest additions'
 echo
 sudo /cdrom/VBoxLinuxAdditions.run
-
-echo
-echo '***Finished'
-echo

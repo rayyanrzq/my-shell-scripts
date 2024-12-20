2319cc95d8dd4a7fb2a51fa7b65bfa932849d5b8
rayyan <rayyanrazaq26@gmail.com>
2024-12-21 01:39:09 +0700

    add text

--- vbox-install.sh
+++ vbox-install.sh
@@ -3,23 +3,29 @@
 # This script spesifically install vbox 7.1.4 for Ubuntu 24.04 LTS
 #
 
-# Change directory
+# Directory to save downloaded package
 cd ~/Downloads
 
-# Install necessary package
+echo
+echo 'Installing necessary package'
+echo
 sudo apt install gcc make perl liblzf1 libqt6core6t64 libqt6dbus6t64 libqt6gui6t64 libqt6help6 libqt6printsupport6t64 libqt6statemachine6 libqt6widgets6t64 libqt6xml6t64 libtpms0
 
-# Download vbox
+echo
+echo 'Downloading VBox'
+echo
 wget https://download.virtualbox.org/virtualbox/7.1.4/virtualbox-7.1_7.1.4-165100~Ubuntu~noble_amd64.deb
 
-# Install vbox
+echo
+echo 'Installing Vbox'
+echo
 sudo dpkg -i virtualbox-*
 
-# Add user to vboxusers group
+echo
+echo 'Add user to vbousers group'
+echo
 sudo usermod -aG vboxusers $USER
 
-# pretty clear
-reboot
 
 
 

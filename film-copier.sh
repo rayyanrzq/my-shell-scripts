e60a64dcd87285ad8eecd7d3880b983186c1667e
rayyanrzq <rayyanrazaq26@gmail.com>
2024-12-20 12:05:30 +0700

    change exclude directory

--- film-copier.sh
+++ film-copier.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#  film-copier - Copy the latest movies on my local directory to
-#  my external hdd
+#  film-copier - copy the latest movies on local directory to
+#  external hdd with rysnc copying tool.
 #
 
 # Directory path
@@ -9,4 +9,4 @@ origin="/home/rayyan/Videos/"
 dest="/media/rayyan/78EC3A9BEC3A541A/Film"
 
 # Copy with rsync
-sudo rsync --archive --verbose --progress --ignore-existing --exclude 'Camera/' $origin $dest
+sudo rsync --archive --verbose --progress --ignore-existing --exclude 'Webcam/' $origin $dest
